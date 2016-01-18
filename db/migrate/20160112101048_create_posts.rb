b4780d1ffa147614bb2683bfd7832384acc8179b
Candy Goodison <candy.goodison@redbubble.com>
2016-01-19 09:24:04 +1100

    fixing some db issues

--- db/migrate/20160112101048_create_posts.rb
+++ db/migrate/20160112101048_create_posts.rb
@@ -1,6 +1,6 @@
 class CreatePosts < ActiveRecord::Migration
   def change
-    create_table :post do |t|
+    create_table :posts do |t|
       t.string :name
       t.string :title
       t.text :content
