0bc1c71676bf6ccdb19687620443edc20fc9b92f
Inksprout <candy.g24@gmail.com>
2016-02-04 21:11:10 +1100

    Facebook login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -240,7 +240,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   config.omniauth :facebook, "808653675927874", "da1535d7e8076d76baeadc9219b0be9d",
-                  callback_url: "CALLBACK_URL"
+                  callback_url: "http://localhost:3000/users/auth/facebook/callback"
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
